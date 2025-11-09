60aee64eaca1f01f15c2cce2055a0d3e352654fd
Angel Melendez <angel99.melendez@gmail.com>
2025-11-08 23:36:05 -0600

    Agregando biblioteca Gson para convertir el formato Json a una clase en java y creando un Record para flexibilizar la convesrción de los datos del Json a los de la clase Titulo.

---
+++ src/com/angel/screenmatch/modelos/TituloOMDb.java
@@ -0,0 +1,4 @@
+package com.angel.screenmatch.modelos;
+
+public record TituloOMDb(String title, String year, String runtime) {
+}
--- src/com/angel/screenmatch/principal/PrincipalConBusqueda.java
+++ src/com/angel/screenmatch/principal/PrincipalConBusqueda.java
@@ -1,5 +1,11 @@
 package com.angel.screenmatch.principal;
 
+import com.angel.screenmatch.modelos.Titulo;
+import com.angel.screenmatch.modelos.TituloOMDb;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -21,6 +27,16 @@ public class PrincipalConBusqueda {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println(response.body());
+        String json = response.body();
+        System.out.println(json);
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .create();
+        TituloOMDb miTituloOmdb = gson.fromJson(json, TituloOMDb.class);
+
+        System.out.println(miTituloOmdb);
+        Titulo miTitulo = new Titulo(miTituloOmdb);
+        System.out.println(miTitulo);
     }
 }
